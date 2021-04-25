0098dc1581f264c0d9dbdebc655d63de5d2bb515
kephen <kevin.nguyen2511@gmail.com>
2021-04-25 15:57:12 +1000

    Perseverance Title Screen + Typing Effect, Added Font

---
+++ 3DCA-Final/Assets/Scripts/UI/TypingEffect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypingEffect : MonoBehaviour
+{
+    // Code used from https://unitycoder.com/blog/2015/12/03/ui-text-typewriter-effect-script/
+
+    Text txt;
+    string storedText;  
+
+    void Awake()
+    {
+        txt = GetComponent<Text>();
+        storedText = txt.text;
+        txt.text = "";
+
+        StartCoroutine("PlayText");
+    }
+
+    IEnumerator PlayText()
+    {
+        foreach(char c in storedText)
+        {
+            txt.text += c;
+            yield return new WaitForSeconds(0.125f);
+        }
+    }
+}
