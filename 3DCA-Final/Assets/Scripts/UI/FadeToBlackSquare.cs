50f93b4be415b0b873b13b5a24d1b583a4785d89
kephen <kevin.nguyen2511@gmail.com>
2021-04-29 13:40:32 +1000

    Fade to black script, Text fade in and out script

---
+++ 3DCA-Final/Assets/Scripts/UI/FadeToBlackSquare.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeToBlackSquare : MonoBehaviour
+{
+    public GameObject blackOutSquare;
+    public float fadeSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StartCoroutine(FadeBlackOutSquare(true, fadeSpeed));
+        }
+    }
+
+    public IEnumerator FadeBlackOutSquare(bool fadeToBlack, float fadeSpeed)
+    {
+        Color objectColor = blackOutSquare.GetComponent<Image>().color;
+        float fadeAmount;
+
+        if (fadeToBlack)
+        {
+            while(blackOutSquare.GetComponent<Image>().color.a < 1)
+            {
+                fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                blackOutSquare.GetComponent<Image>().color = objectColor;
+                yield return null;
+            }
+        }
+
+        yield return new WaitForEndOfFrame();
+    }
+}
