be9ea43ce6b9601eff69ac2828bca02ddf3244e0
kephen <kevin.nguyen2511@gmail.com>
2021-05-06 16:46:44 +1000

    Perseverance First Person HUD

---
+++ 3DCA-Final/Assets/Scripts/UI/RandomFillAmount.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomFillAmount : MonoBehaviour
+{
+    private Image fillTarget;
+    public float minAmount;
+    public float maxAmount;
+
+    public float fillDelay;
+    private bool isRunning;
+
+    void Start()
+    {
+        fillTarget = gameObject.GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        if (!isRunning)
+            StartCoroutine(RandomFill());
+
+    }
+
+    IEnumerator RandomFill()
+    {
+        isRunning = true;
+        float fillValue = Random.Range(minAmount, maxAmount);
+        fillTarget.fillAmount = fillValue;
+        yield return new WaitForSeconds(fillDelay);
+        isRunning = false;
+    }
+}
