fe97bcecee4a1a4dbd7f5ed0b91172cf6c717054
biggles65 <39050208+biggles65@users.noreply.github.com>
2021-05-12 15:53:16 +1000

    skyscene
    fix

---
+++ 3DCA-Final/Assets/snd_planetpass.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snd_planetpass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource audioSource;
+    public float delay = 4;
+
+    void Start()
+    {
+        // Plays an Audio Clip after 4 seconds
+        audioSource.PlayDelayed(delay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
