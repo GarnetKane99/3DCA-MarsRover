6a85f0dc25409d5452d9c89f3423d72ba58a0b9b
GarnetKane99 <75872930+GarnetKane99@users.noreply.github.com>
2021-05-12 17:22:28 +1000

    perseverance movement

--- 3DCA-Final/Assets/JakeSceneCameraEffect.cs
+++ 3DCA-Final/Assets/JakeSceneCameraEffect.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 public class JakeSceneCameraEffect : MonoBehaviour
 {
     public Camera mainCam;
+    public Transform CameraSwap;
+    float maxRotateTime = 5.0f;
+    float maxLookTime = 8.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCam = GetComponent<Camera>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //mainCam.transform.rotation();
+        if (maxRotateTime > 0)
+        {
+            maxRotateTime -= Time.deltaTime;
+            mainCam.transform.Rotate(0, -5 * Time.deltaTime, 0, Space.World);
+        }
+
+        if(maxLookTime > 0)
+        {
+            maxLookTime -= Time.deltaTime;
+        }
+        if(maxLookTime <= 0)
+        {
+            mainCam.transform.position = CameraSwap.position;
+            //mainCam.transform.rotation = Quaternion.Euler(-1.5f, -90f, 0);
+            mainCam.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
---
+++ 3DCA-Final/Assets/perseverancewheels.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class perseverancewheels : MonoBehaviour
+{
+    public GameObject Wheels;
+    public WheelCollider WC;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WC = GetComponent<WheelCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion Rotation;// = WC.transform.rotation;
+        Vector3 position;// = WC.transform.position;
+
+
+        WC.GetWorldPose(out position, out Rotation);
+
+        Wheels.transform.position = position;
+        Wheels.transform.rotation = Rotation;
+    }
+}
