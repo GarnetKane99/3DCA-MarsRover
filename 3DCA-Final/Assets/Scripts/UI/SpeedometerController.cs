370c409f80d7cee6e2517f96a8a0ad9b3ab59135
kephen <kevin.nguyen2511@gmail.com>
2021-05-16 19:35:19 +1000

    Speedometer in interactive sequence

---
+++ 3DCA-Final/Assets/Scripts/UI/SpeedometerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedometerController : MonoBehaviour
+{
+    public Text spdTxt;
+    public Image spdImg;
+
+    bool isGainingSpeed = false;
+    bool isDroppingSpeed = false;
+    bool isCurrentlyGaining = false;
+
+    int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            isGainingSpeed = true;
+        }
+        if (Input.GetKeyUp("w"))
+        {
+            isGainingSpeed = false;
+        }
+
+        if(isGainingSpeed && !isCurrentlyGaining)
+        {
+            StartCoroutine(GainSpeed());
+        }
+
+        if (!isGainingSpeed && !isDroppingSpeed)
+        {
+            StartCoroutine(DropSpeed());
+        }
+
+        speed = Mathf.Clamp(speed, 0, 20);
+        spdTxt.text = speed.ToString();
+        //spdImg.fillAmount = (speed * 5) / 100;
+        spdImg.fillAmount = 1;
+    }
+
+    IEnumerator DropSpeed()
+    {
+        isDroppingSpeed = true;
+        speed -= 1;
+        yield return new WaitForSeconds(1f);
+        isDroppingSpeed = false;
+    }
+
+    IEnumerator GainSpeed()
+    {
+        isCurrentlyGaining = true;
+        speed += 1;
+        yield return new WaitForSeconds(0.5f);
+        isCurrentlyGaining = false;
+    }
+}
