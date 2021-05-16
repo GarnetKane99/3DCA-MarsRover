df23b9bf49537092d924fbf38a0e59bef2fb92ff
kephen <kevin.nguyen2511@gmail.com>
2021-05-16 18:39:33 +1000

    speedometer text scene 4, fixed transparent screens in mission control scene

---
+++ 3DCA-Final/Assets/Scripts/UI/SpeedometerTxt.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedometerTxt : MonoBehaviour
+{
+    private Text speedometerTxt;
+    float speedometerValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedometerTxt = GetComponent<Text>();
+        StartCoroutine(Speedometer());   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedometerTxt.text = "0" + speedometerValue.ToString();
+    }
+
+    IEnumerator Speedometer()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            speedometerValue = Random.Range(3, 5);
+            yield return new WaitForSeconds(0.5f);
+        }
+        speedometerValue = 1;
+        yield return new WaitForSeconds(1f);
+        speedometerValue = 0;
+
+    }
+}
